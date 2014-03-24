53c1f948fdecc42668f5c8d7874d98bb5b2bba8d
zaher merna <mzaher@student.tgm.ac.at>
2014-03-24 09:34:16 +0100

    --- comit ...

--- src/AU08/hallo.java
+++ src/AU08/hallo.java
@@ -4,9 +4,8 @@ public class hallo {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hallo");
-		System.out.println("Hallo");
-		System.out.println("Hallo");
+		System.out.println("Hallo , Wie gehts? ");
+	
 
 	}
 
